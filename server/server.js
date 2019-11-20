4ef69e46c46f1fb0ba6e618ea66cce918a77ff87
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-11-20 14:55:06 -0600

    create routes on server side

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const teamRouter = require('./routes/team.router');
+const rosterRouter = require('./routes/roster.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,7 +26,8 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-app.use('/api/teams', teamRouter)
+app.use('/api/teams', teamRouter);
+app.use('/api/roster', rosterRouter)
 
 // Serve static files
 app.use(express.static('build'));
