d9c217aae2e0ea490a3fddab86282091e788fd49
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-11-21 15:08:30 -0600

    back button to return to roster page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,14 +3,25 @@ import { connect } from 'react-redux';
 
 class AddTeam extends Component {
 
+    handleBackButton = () => {
+        console.log('Back to roster');
+    
+        this.props.history.push('/roster')
+    }
 
     render() {
         return (
             <div>
-                <h3>Profile</h3>
+                <button onClick={this.handleBackButton}>Back to Roster</button>
+                <h3>Profile</h3> 
                 {this.props.profileReducer.skater_name}
                 <br />#{this.props.profileReducer.number}
                 <br />Position: {this.props.profileReducer.position}
+
+                <p>Practices Attended</p>
+                <ul>
+                    <li>date - attendance type</li>
+                </ul>
 
                 <pre>{JSON.stringify(this.props, null, 2)}</pre>
             </div>
