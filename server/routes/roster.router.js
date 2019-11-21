7e2a0527f97ec04f443e9d0ab8d7ce42f29b9e5d
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-11-21 13:30:12 -0600

    display skater details on page

--- server/routes/roster.router.js
+++ server/routes/roster.router.js
@@ -3,44 +3,47 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 
-router.get('/:id', (req, res) => {
+router.get('/', (req, res) => {
     //brings teams from database to client side
     console.log('GET /api/details');
-    pool.query(`SELECT * FROM "skaters" WHERE "team_id"=$1`, [req.params.id]).then((result) => {
+    pool.query(`SELECT * FROM "skaters"`).then((result) => {
         res.send(result.rows);
     }).catch((error) => {
-        console.log('Error GET /api/rosters', error)
+        console.log('Error GET /api/roster', error)
         res.sendStatus(500);
     });
 });
 
 
-// router.post('/', (req, res) => {
-//     //takes inputted value from addTeam page and adds to the database
-//     const newTeam = req.body.team;
-//     console.log('new team',req.body);
-//     const queryText = `INSERT INTO teams ("team_name")
-//                       VALUES ($1)`;
-//     const queryValues = [
-//         newTeam,
-//     ];
-//     pool.query(queryText, queryValues)
-//         .then(() => { res.sendStatus(201); })
-//         .catch((err) => {
-//             console.log('Error completing INSERT team query', err);
-//             res.sendStatus(500);
-//         });
-// });
+router.post('/', (req, res) => {
+    //takes inputted value from addSkater page and adds to the database
+    const newSkater = req.body;
+    console.log('new skater',req.body);
+    const queryText = `INSERT INTO skaters ("skater_name", "team_id", "number", "position")
+                      VALUES ($1, $2, $3, $4)`;
+    const queryValues = [
+        newSkater.name,
+        newSkater.team_id,
+        newSkater.number,
+        newSkater.position,
+    ];
+    pool.query(queryText, queryValues)
+        .then(() => { res.sendStatus(201); })
+        .catch((err) => {
+            console.log('Error completing INSERT skater query', err);
+            res.sendStatus(500);
+        });
+});
 
-// router.delete('/:id', (req, res) => {
-//     //removes user selected team from database
-//     const queryText = 'DELETE FROM teams WHERE id=$1';
-//     pool.query(queryText, [req.params.id])
-//       .then(() => { res.sendStatus(200); })
-//       .catch((err) => {
-//         console.log('Error completing DELETE team query', err);
-//         res.sendStatus(500);
-//       });
-//   });
+router.delete('/:id', (req, res) => {
+    //removes user selected skater from database
+    const queryText = 'DELETE FROM skaters WHERE id=$1';
+    pool.query(queryText, [req.params.id])
+      .then(() => { res.sendStatus(200); })
+      .catch((err) => {
+        console.log('Error completing DELETE skater query', err);
+        res.sendStatus(500);
+      });
+  });
 
 module.exports = router;
--- src/redux/sagas/rosterSaga.js
+++ src/redux/sagas/rosterSaga.js
@@ -2,25 +2,44 @@ import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
 function* rosterSaga() {
+    yield takeLatest('POST_SKATER', addSkater);
     yield takeLatest('GET_ROSTER', rosterDetails);
+    yield takeLatest('DELETE_SKATER', removeSkater);
 }
 
-// function* rosterDetails(action) {
-//     // gets all skaters associated with that team id
-//     try {
-//         const rosterResponse = yield axios.get('/api/roster');
-//         yield put({ type: 'SET_ROSTER', payload: rosterResponse.data });
-//     } catch (error) {
-//         console.log('error fetching roster', error);
-//     }
-// }
+function* addSkater(action) {
+    //sends inputted user value from AddSkater to server side post route
+    try {
+        yield axios.post('/api/roster',  action.payload);
+        yield put({ type: 'GET_ROSTER' })
+    } catch (error) {
+        console.log('error posting skater', error);
+    }
+}
 
 function* rosterDetails(action) {
+    //communicates with server route to get skaters from database
     console.log('roster details', action);
-    const rosterResponse = yield axios.get(`/api/roster/${action.payload.id}`);
+    try {
+    const rosterResponse = yield axios.get(`/api/roster/`);
     yield put({ type: 'SET_ROSTER', payload: rosterResponse.data});
+    console.log('rosterDetails was hit with action:', action);
+    } catch (error) {
+        console.log('error fetching skaters', error);
+    }
+}
+
+function* removeSkater(action) {
+    //communicates with server side to remove skater from database
+    try {
+        yield axios.delete(`/api/roster/${action.payload}`);
+        yield put({ type: 'GET_ROSTER' });
+    } catch (error) {
+        console.log('error deleting skater', error);
+    }
+}
     // const movieGenresResponse = yield axios.get(`/api/movies/genres/${action.payload.id}`);
     // yield put({ type: 'SET_GENRES', payload: movieGenresResponse.data});
-}
+
 
 export default rosterSaga;
