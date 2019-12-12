4029b74e29ecbb7169b327f09984cbe57e0dd2f8
Sharifa Simon <sharifa.a.simon@gmail.com>
2019-12-12 16:06:39 -0600

    add comments, remove console logs, formatting for AddTeam component

--- src/components/AddTeam/AddTeam.js
+++ src/components/AddTeam/AddTeam.js
@@ -12,19 +12,14 @@ class AddTeam extends Component {
         team: ''
     }
 
-    // handleCancelClick = () => {
-    //     //takes user back to Team page
-    //     console.log('Cancel clicked');
-    // }
-
     handleAddTeam = () => {
         //adds new Team to Database and returns user to teams page
-        console.log('Adding Team');
         this.props.dispatch({ type: 'POST_TEAM', payload: this.state.team });
         this.props.history.push('/teams');
     }
 
     handleCancel = () => {
+        //moves user back to teams component
         this.props.history.push('/teams');
     }
 
@@ -44,23 +39,21 @@ class AddTeam extends Component {
                 <h3>Add New Team</h3>
                 <Paper>
                     <br />
-                <Grid container spacing={1} direction="column" alignItems="center">
+                    <Grid container spacing={1} direction="column" alignItems="center">
 
-                    <TextField id="outlined-basic" label="Team Name" variant="outlined"
-                        type="text"
-                        name="team"
-                        value={this.state.team} onChange={this.handleInputChangeForNewTeam} />
+                        <TextField id="outlined-basic" label="Team Name" variant="outlined"
+                            type="text"
+                            name="team"
+                            value={this.state.team} onChange={this.handleInputChangeForNewTeam} />
 
-                    <Grid item xs={12} md={6}>
-                       
-                        <ButtonGroup variant="contained"
-                            color="primary" aria-label="full width outlined button group" size="small">
+                        <Grid item xs={12} md={6}>
 
-                            <Button onClick={this.handleAddTeam}>Add Team</Button>
-
-                        </ButtonGroup>
+                            <ButtonGroup variant="contained"
+                                color="primary" aria-label="full width outlined button group" size="small">
+                                <Button onClick={this.handleAddTeam}>Add Team</Button>
+                            </ButtonGroup>
+                        </Grid>
                     </Grid>
-                </Grid>
                 </Paper>
             </div>
 
